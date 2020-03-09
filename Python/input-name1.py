47765af56afd47e70a9b56d42bccdd014abc90b0
Carlos González <33579307+carlosgm02@users.noreply.github.com>
2020-03-09 16:25:21 -0600

    Update input-name1.py

--- Python/input-name1.py
+++ Python/input-name1.py
@@ -2,6 +2,7 @@ print("¿Cuál es tu nombre?")
 # "print" imprimirá la pregunta
 
 nombre = input()
+# "input(x)" generará un cuadro de entrada
 # "nombre" será declarado con el valor que se ingrese con input
 
 print(f"Buenos días, {nombre}")
