bea21f819f27d94b41338a26292954395b6bfc75
Carlos González <33579307+carlosgm02@users.noreply.github.com>
2020-03-09 16:20:01 -0600

    Update if-oproot1.py

--- Python/if-oproot1.py
+++ Python/if-oproot1.py
@@ -1,10 +1,13 @@
 import math
+# Esto importará las funciones matemáticas
 
 REAL = float(input("Dame un número real: "))
+# float(x) dejará que el número tenga decimales
 
 if REAL < 0:
     root = "imaginario"
     print(f"La raíz cuadrada del número introducido es un número {root}")
+    # root se debe especificar en 
 else:
     root = math.sqrt(REAL)
     print(f"La raíz cuadrada del número introducido es {round(root, 2)}")
