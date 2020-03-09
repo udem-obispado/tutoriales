0d11ef8c63ad299bffbd0e2f8e852815d1852663
Carlos González <33579307+carlosgm02@users.noreply.github.com>
2020-03-09 17:26:20 -0600

    Update if-trgletype1.py

--- Python/if-trgletype1.py
+++ Python/if-trgletype1.py
@@ -5,13 +5,18 @@ L3 = float(input("Ingresa el tercer lado: "))
 if L1 == L2:
     if L2 == L3:
         print(f"El triángulo es equilátero")
+        # "print" imprimirá el texto o resultado
     else:
         print(f"El triángulo es isósceles")
+        # "print" imprimirá el texto o resultado
 else:
     if L2 == L3:
         print(f"El triángulo es isósceles")
+        # "print" imprimirá el texto o resultado
     else:
         if L1 == L3:
             print(f"El triángulo es isósceles")
+            # "print" imprimirá el texto o resultado
         else:
             print(f"El triángulo es escaleno")
+            # "print" imprimirá el texto o resultado
