d06213114115930319769df87f86e2ed7648ddfc
Carlos González <33579307+carlosgm02@users.noreply.github.com>
2020-03-11 17:03:14 -0600

    Update if-operatns1.py

--- Python/if-operatns1.py
+++ Python/if-operatns1.py
@@ -8,12 +8,14 @@ if OP == "S":
 elif OP == "R":
     RS = N1-N2
     print(f"La resta de los dos números es: {RS}")
+    # elif se usa para anidar una condición en la condición principal.
 elif OP == "M":
     RS = N1*N2
     print(f"El producto de los dos números es: {RS}")
+    # elif se usa para anidar una condición en la condición principal.
 elif OP == "D":
     RS = N1/N2
     print(f"El cociente de los dos números es: {RS}")
+    # elif se usa para anidar una condición en la condición principal.
 else:
-    print("La letra que has ingresado no es válida. S para sumar, R para restar, M para multiplicar o D para dividir")
-    
+    print("La letra que has ingresado no es válida. Te sugiero que escribas S para sumar, R para restar, M para multiplicar o D para dividir")
