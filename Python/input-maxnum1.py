28fe7ac59b51bf1a2d2927f93a4e58e851ba4d9b
Carlos González <33579307+carlosgm02@users.noreply.github.com>
2020-03-11 16:32:32 -0600

    Update input-maxnum1.py

--- Python/input-maxnum1.py
+++ Python/input-maxnum1.py
@@ -1,5 +1,9 @@
 N1 = float(input("Ingresa el primer número: "))
 N2 = float(input("Ingresa el segundo número: "))
 R = max(N1,N2)
+# "float(x)" dejará que el número tenga decimales
+# "input(x)" generará un cuadro de entrada
+# "max(x,y)" encontrará el número mayor
 
 print(f"De los números {N1} y {N2} el más grande es {R}")
+# "print" imprimirá el texto o resultado
